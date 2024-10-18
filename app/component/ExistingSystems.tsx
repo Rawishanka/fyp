ebd9d09a9a344f22a24d43bb681037c88c7fab71
rasinduRawishanka <63003929+Rawishanka@users.noreply.github.com>
2024-10-18 12:33:03 +0530

    existing systems photos

--- app/component/ExistingSystems.tsx
+++ app/component/ExistingSystems.tsx
@@ -2,26 +2,17 @@ import React from "react";
 import Image from "next/image";
 const ExistingSystems = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold text-blue-700 mb-4">
+    <div className=" container mx-auto md:flex-row items-center">
+      <h1 className="text-3xl font-bold text-blue-700 mb-4  ">
         Existing Systems
       </h1>
-      <div className="flex container mx-auto flex-col md:flex-row items-center justify-center">
-        <div>
-          <ul>
-            <li>d</li>
-            <li>d</li>
-            <li>d</li>
-          </ul>
-        </div>
-        <div>
-          <ul>
-            <li>e</li>
-            <li>e</li>
-            <li>e</li>
-          </ul>
-        </div>
-        <Image src={""} alt={""}></Image>
+      <div className="grid grid-cols-3 gap-4 mt-10">
+        <div><Image src={"/gumtree.png"} width={200} height={300} alt={"gumtree"}></Image></div>
+        <div><Image src={"/craiglist.png"} alt={"gumtree"} width={300} height={300}></Image></div>
+        <div><Image src={"/facebook.png"} width={300} height={300} alt={"gumtree"}></Image></div>
+        <div><Image src={"/ikman.png"} alt={"gumtree"} width={300} height={200}></Image></div>
+        <div><Image src={"/oodle.png"} width={300} height={300} alt={"gumtree"}></Image></div>
+        <div><Image src={"/trovit.png"} alt={"gumtree"} width={300} height={200}></Image></div>
       </div>
     </div>
   );
