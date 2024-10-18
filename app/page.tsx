405eaa9d115b0f4d76f9d44bf8d0e9884c0c53cd
rasinduRawishanka <63003929+Rawishanka@users.noreply.github.com>
2024-10-18 11:43:41 +0530

    change welcome

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,69 @@
 import { Header } from "@/app/component/Header";
 import { Section } from "@/app/component/Section";
 import Image from "next/image";
-import welcome from "../public/welcome.png";
 export default function Home() {
   return (
     <div>
       <Header />
       <main>
         <section className="flex items-center justify-center w-auto">
-          <Image
-            src="/welcome.png"
-            alt="Welcome Image"
-            width={1500}  // Increased width
-    height={500} // Increased height 
-            className="rounded-lg"
-          />
+          <div>
+            <Image
+              src="/classified_adv.png"
+              alt="Welcome Image"
+              width={600} // Increased width
+              height={500} // Increased height
+              className="rounded-lg"
+            />
+          </div>
+          <div className="px-6">
+            <h1 className="text-6xl">
+              Resource Allocator/Mapper
+              <br /> for
+              <br /> Efficient Transactions
+            </h1>
+            <div className="mt-20">
+              <ul>
+                <li>Supervisor : Prof. Indika Perera</li>
+                <li>
+                Team Members:
+                  <ul>
+                    <li className="pl-16">Alwis L.W.D.D 200021T</li>
+                    <li className="pl-16">Chathunka W.A.P 200089G</li>
+                    <li className="pl-16">Rawishanka D.D.R.L 200540H</li>
+                  </ul>
+                </li>
+              </ul>
+            </div>
+          </div>
         </section>
-
+        <section id="introduction" className="py-8">
+          <div className="container mx-auto md:flex-row items-center">
+            <h1 className="text-3xl font-bold text-blue-700 mb-4">
+              Introduction to Resource Allocation in Digital Marketplaces
+            </h1>
+            <p className="py-5 text-2xl text-orange-400">
+              Involves the process of efficiently connecting users’ needs with
+              available resources through various online platforms
+            </p>
+            <ul>
+              <li>
+                <b>User Empowerment:</b> Enhances the ability of individuals and
+                businesses to quickly and accurately find what they need.
+              </li>
+              <li>
+                <b>Economic Efficiency:</b> Optimizes market operations, drives
+                economic growth, and improves the effectiveness of online
+                transactions
+              </li>
+              <li>
+                <b>Global Reach:</b> Facilitates access to resources on a global
+                scale, bridging gaps between supply and demand in diverse
+                markets.
+              </li>
+            </ul>
+          </div>
+        </section>
         <Section
           id="introduction"
           title="Introduction to Resource Allocation in Digital Marketplaces"
